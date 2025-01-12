2b706812e0be28fe32c59d240f84923fba48037a
offic <offic@DESKTOP-GLF4UNU>
2025-01-12 18:43:51 +0600

    1st commit

---
+++ src/hello_world/Hello_world.java
@@ -0,0 +1,21 @@
+package hello_world;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// String declare system -1------------------------
+		String str = "Java Developer";
+		String string = new String(str);
+		System.out.println(string);
+
+		// String declare system -2------------------------
+		String str1 = new String("Android Developer.");
+		System.out.println(str1);
+
+		// String declare system -3------------------------
+		char[] str2 = { 'S', 'u', 'c', 'c', 'e', 's', 's' };
+		System.out.println(str2);
+
+	}
+
+}
