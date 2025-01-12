3b4001ecfe1bbd60aa0822ffebb45228df430c1a
offic <offic@DESKTOP-GLF4UNU>
2025-01-12 18:51:38 +0600

    2nd commit

--- src/hello_world/Hello_world.java
+++ src/hello_world/Hello_world.java
@@ -8,10 +8,14 @@ public class Hello_world {
 		String string = new String(str);
 		System.out.println(string);
 
+		
+		
 		// String declare system -2------------------------
 		String str1 = new String("Android Developer.");
 		System.out.println(str1);
 
+		
+		
 		// String declare system -3------------------------
 		char[] str2 = { 'S', 'u', 'c', 'c', 'e', 's', 's' };
 		System.out.println(str2);
